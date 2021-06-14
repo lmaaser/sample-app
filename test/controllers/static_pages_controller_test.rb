fce7854fbe090275c579349e300369861ce3f861
Lauren Maaser <lauren.maaser@freshly.com>
2021-06-14 15:59:49 -0700

    change syntax

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,17 +11,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home page' do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', 'Home | #{ @base_title }'
+    assert_select 'title', "Home | #{ @base_title }"
   end
 
   test 'should get help page' do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', 'Help | #{ @base_title }'
+    assert_select 'title', "Help | #{ @base_title }"
   end
   test 'should get about page' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'About | #{ @base_title }'
+    assert_select 'title', "About | #{ @base_title }"
   end
 end
