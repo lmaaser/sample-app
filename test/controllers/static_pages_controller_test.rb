6d8b9b13cd5f5c493752055e56c32dec4d9c7d6a
Lauren Maaser <lauren.maaser@freshly.com>
2021-06-14 15:13:07 -0700

    new repo

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
-  
-  test "should get home page" do
+  test 'should get root of app' do
+    get root_url
+    assert_response :success
+  end
+  test 'should get home page' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{ @base_title }"
+    assert_select 'title', 'Home | #{ @base_title }'
   end
 
-  test "should get help page" do
+  test 'should get help page' do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{ @base_title }"
+    assert_select 'title', 'Help | #{ @base_title }'
   end
-  
-  test "should get about page" do
+  test 'should get about page' do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{ @base_title }"
+    assert_select 'title', 'About | #{ @base_title }'
   end
 end
